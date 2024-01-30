610e102c3111512e4f2eebfc85923424c7c4106e
dbseend <shyoon840@gmail.com>
2024-01-30 23:51:56 +0900

    프로젝트 생성ㅇ

---
+++ src/main/java/com/winter/WonSsoGong/WonSsoGongApplication.java
@@ -0,0 +1,13 @@
+package com.winter.WonSsoGong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonSsoGongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonSsoGongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winter/WonSsoGong/WonSsoGongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winter.WonSsoGong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonSsoGongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
